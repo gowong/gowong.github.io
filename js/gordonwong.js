d934adebfbb775dc826e694751c5c4034cd5d0fa
Gordon Wong <gwong001@gmail.com>
2016-01-10 23:24:39 +0800

    Automatically show tabs indicated in the URL hash

--- js/gordonwong.js
+++ js/gordonwong.js
@@ -1,6 +1,16 @@
+
+function showTab(tabId) {
+  $('#tabs a[href="' + tabId + '"]').tab('show');
+}
+
 $(function() {
+  // Show tab indicated by the URL hash
+  if(window.location.hash) {
+      showTab(window.location.hash);
+  }
+
   // Select news tab when clicking link in footer
   $('#footer-news-link').click(function (e) {
-    $('#tabs a[href="#news"]').tab('show');
+    showTab('#news');
   });
 });
