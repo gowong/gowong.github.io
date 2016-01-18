9a634b8bb00c64cf265ec58e50c03ebf40b1b04e
Gordon Wong <gwong001@gmail.com>
2016-01-17 18:30:41 -0800

    Show experience tab when clicking a software thumbnail

--- js/gordonwong.js
+++ js/gordonwong.js
@@ -3,14 +3,26 @@ function showTab(tabId) {
   $('#tabs a[href="' + tabId + '"]').tab('show');
 }
 
+function registerSoftwareThumbnailClickHandlers() {
+  // Show experience tab when clicking a software thumbnail
+  var clickHandler = function() {
+    showTab('#experience');
+  };
+  $('.software-thumbnail').each(function() {
+    $(this).click(clickHandler);
+  });
+}
+
 $(function() {
   // Show tab indicated by the URL hash
-  if(window.location.hash) {
+  if (window.location.hash) {
       showTab(window.location.hash);
   }
 
-  // Select news tab when clicking link in footer
-  $('#footer-news-link').click(function (e) {
+  // Show news tab when clicking link in footer
+  $('#footer-news-link').click(function() {
     showTab('#news');
   });
+
+  registerSoftwareThumbnailClickHandlers();
 });
