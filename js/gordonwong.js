c5880761a00997b06947bcb1ab5a023fa60a6e8a
Gordon Wong <gwong001@gmail.com>
2016-01-21 22:40:33 -0800

    Zoom images when clicked

--- js/gordonwong.js
+++ js/gordonwong.js
@@ -3,6 +3,25 @@ function showTab(tabId) {
   $('#tabs a[href="' + tabId + '"]').tab('show');
 }
 
+function registerLightboxHandlers() {
+  var lightbox = lity();
+
+  // Lightbox must be opened programmatically to be controlled programmatically
+  $(document).on('click', '[data-lightbox]', function(event) {
+    lightbox(event.target.src);
+  });
+
+  // Listen for open events
+  $(document).on('lity:open', function(event, elLightbox, trigger) {
+    // Close when clicking the image (default behavior only closes when
+    // clicking outside of the image)
+    $(elLightbox).on('click', lightbox.close);
+  });
+
+  // Close when scrolling the window
+  $(window).on('scroll', lightbox.close);
+}
+
 function registerSoftwareThumbnailClickHandlers() {
   // Show experience tab when clicking a software thumbnail
   var clickHandler = function() {
@@ -40,5 +59,6 @@ $(function() {
   });
 
   registerSoftwareThumbnailClickHandlers();
+  registerLightboxHandlers();
   setupExperienceCarousels();
 });
